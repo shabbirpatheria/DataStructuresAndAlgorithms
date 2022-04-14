6a57e4bc3496e434731d42be09d73f249c3817a8
Shabbir Patheria <shabbir.patheria@hotmail.com>
2022-04-14 13:28:49 +0530

    Array Basics

--- DataStructuresAndAlgorithms/main.cpp
+++ DataStructuresAndAlgorithms/main.cpp
@@ -1,14 +1,28 @@
-//
-//  main.cpp
-//  DataStructuresAndAlgorithms
-//
-//  Created by Shabbir Patheria on 14/04/22.
-//
-
 #include <iostream>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    cout << "Hello World" << endl;
+int main(){
+    
+    int n;
+    cout << "Enter Size: ";
+    cin >> n;
+    int A[n];
+    //int A[n] = {0,2,3,4,5} will throw error
+    for(int i = 0; i<n; i++){
+        A[i] = i*i;
+    }
+    //4 Bytes for int
+    cout << sizeof(A) << endl;
+    
+    for(int i =0; i< n; i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    
+    //C++
+    for(int x:A){
+        cout << x << " ";
+    }
+    cout << endl;
     return 0;
 }
