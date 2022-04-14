4055851401a8bd09321c38fe8eb44feac4273e6e
Shabbir Patheria <shabbir.patheria@hotmail.com>
2022-04-14 13:04:45 +0530

    Initial Commit

---
+++ DataStructuresAndAlgorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructuresAndAlgorithms
+//
+//  Created by Shabbir Patheria on 14/04/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello World" << endl;
+    return 0;
+}
