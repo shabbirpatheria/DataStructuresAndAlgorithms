55f2e9a493b1633924af6a53df82e0624191a8fb
Shabbir Patheria <shabbir.patheria@hotmail.com>
2022-05-09 13:33:05 +0530

    Printing all Pairs

--- DataStructuresAndAlgorithms/main.cpp
+++ DataStructuresAndAlgorithms/main.cpp
@@ -3,15 +3,17 @@
 
 using namespace std;
 
-void reverseArray(int arr[], int n){
-    for(int i =0 ; i < n/2 ; i++){
-        swap(arr[i],arr[n-i-1]);
+void printAllPairs(int arr[], int n){
+    for(int i = 0; i<n; i++){
+        for(int j = i+1; j<n ;j++){
+            cout << "(" << arr[i] << "," << arr[j] << "), ";
+        }
     }
+    cout << endl;
 }
 
 int main(){
     int arr[] = {1,2,3,4,5,6,7,8,9,10};
     int n = sizeof(arr)/sizeof(int);
-    reverseArray(arr, n);
-    printArray(arr, n);
+    printAllPairs(arr,n);
 }
