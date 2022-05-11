edc241a2c2a4a063f4230e4b1d1dc1f6b813b0c2
Shabbir Patheria <shabbir.patheria@hotmail.com>
2022-05-11 23:29:18 +0530

    Largest Subarray Sum (Brute Force)

--- DataStructuresAndAlgorithms/main.cpp
+++ DataStructuresAndAlgorithms/main.cpp
@@ -3,21 +3,29 @@
 
 using namespace std;
 
-void printSubarrays(int arr[], int n){
+int largestSubarraySum(int arr[], int n){
+    
+    int largestSum = -1;
     for(int i =0 ; i<n; i++){
         for(int j =i; j<n; j++){
+            
+            int subarraySum = 0;
             //Gives all the subarray sets
             for(int k = i; k<=j ; k++){
-                cout << arr[k] << ", ";
+                //cout << arr[k] << ", ";
+                subarraySum += arr[k];
             }
-            cout << endl;
+            
+            largestSum = max(subarraySum,largestSum);
         }
-        cout << endl;
     }
+    return largestSum;
 }
 
+
+
 int main(){
-    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int arr[] = {-2,3,4,-1,5,-12,6,1,3};
     int n = sizeof(arr)/sizeof(int);
-    printSubarrays(arr, n);
+    cout << "Largest Sum: " << largestSubarraySum(arr, n) << endl;
 }
