74591d71307f9aa2005b9e8aec393fde79d1f3fa
Shabbir Patheria <shabbir.patheria@hotmail.com>
2022-05-12 23:55:20 +0530

    Largest Subarray Sum (Prefix Method)

--- DataStructuresAndAlgorithms/main.cpp
+++ DataStructuresAndAlgorithms/main.cpp
@@ -2,21 +2,22 @@
 #include "genericFunctions.hpp"
 
 using namespace std;
+//Prefix Sum Approach
 
 int largestSubarraySum(int arr[], int n){
     
-    int largestSum = -1;
-    for(int i =0 ; i<n; i++){
-        for(int j =i; j<n; j++){
-            
-            int subarraySum = 0;
-            //Gives all the subarray sets
-            for(int k = i; k<=j ; k++){
-                //cout << arr[k] << ", ";
-                subarraySum += arr[k];
-            }
-            
-            largestSum = max(subarraySum,largestSum);
+    int prefix[n];
+    prefix[0] = arr[0];
+    for(int i = 1; i<n; i++)
+    {
+        prefix[i] = prefix[i-1]+arr[i];
+    }
+    //Largest Sum Logic
+    int largestSum = 0;
+    for(int i =0; i<n; i++){
+        for(int j = i; j<n; j++){
+            int subarraySum = i>0 ? prefix[j]-prefix[i-1] : prefix[j];
+            largestSum = max(subarraySum, largestSum);
         }
     }
     return largestSum;
