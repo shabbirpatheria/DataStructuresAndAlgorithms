31f11f6cb37568e0b1ba2c6fdd82bae69b8ebd06
Shabbir Patheria <shabbir.patheria@hotmail.com>
2022-05-16 22:52:29 +0530

    Bubble Sort

--- DataStructuresAndAlgorithms/main.cpp
+++ DataStructuresAndAlgorithms/main.cpp
@@ -1,31 +1,19 @@
-#include <iostream>
+#include<iostream>
 #include "genericFunctions.hpp"
-#include<bits/stdc++.h>
-
 using namespace std;
 
+void bubbleSort(int a[], int n){
+    for(int i = 1; i <= n-1; i++){
+        for(int j = 0; j <= n-i-1; j++){
+            if(a[j] > a[j+1]){
+                swap(a[j],a[j+1]);
+            }
+        }
+    }
+}
 int main(){
-    //When size of vector increases,
-    //it creates a new vector 2x the size and copies all the data
-    //from the previos copy and then it'll be destroyed
-    
-    
-    vector<int> arr = {1,2,3,5,7,8,12,23};
-    cout << "Capacity: " << arr.capacity() << endl;
-    //Push_Back O(1)
-    arr.push_back(15);
-    //Number of the elements
-    cout << "Size: " <<  arr.size() << endl;
-    //Capacity of the vector
-    cout << "Capacity: " <<  arr.capacity() << endl;
-    //Print all the elements
-    printVector(arr);
-    
-    arr.pop_back();
-    printVector(arr);
-    cout << "Capacity: " <<  arr.capacity() << endl;
-    
-    //Fill Constructor
-    vector<int> a0(10,7);
-    printVector(a0);
+    int arr[] = {2,4,1,3,5,9,7,6,0};
+    int size = sizeof(arr)/sizeof(int);
+    bubbleSort(arr, size);
+    printArray(arr,size);
 }
