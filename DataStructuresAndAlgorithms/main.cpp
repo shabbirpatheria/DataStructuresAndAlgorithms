5b21fd2823709d44919bb568b9ac0caa0298bff1
Shabbir Patheria <shabbir.patheria@hotmail.com>
2022-05-24 23:59:07 +0530

    Little change to compare function

--- DataStructuresAndAlgorithms/main.cpp
+++ DataStructuresAndAlgorithms/main.cpp
@@ -3,17 +3,12 @@
 #include<algorithm>
 using namespace std;
 
-bool compare(int a, int b){
-    cout << "Comparing " << a << " and " << b << "." << endl;
-    return a>b;
-}
-
 int main(){
     int arr[] = {3,5,2,1,6,9,8,7};
     int n = sizeof(arr)/sizeof(int);
-    sort(arr,arr+n,compare);
+    sort(arr,arr+n,greater<int>());
     //compare is NOT a function call, instead passing a function as a perimeter
-    //Can also use 'greater<int>()' instead of compare
+    //greater<int>() is the change.
     
     cout << "Sorted Array (in Reverse): " << endl;
     printArray(arr,n);
