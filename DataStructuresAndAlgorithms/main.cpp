d6bbd08f0385455df452a24946006b05fc695140
Shabbir Patheria <shabbir.patheria@hotmail.com>
2022-05-28 16:27:00 +0530

    Counting Sort

--- DataStructuresAndAlgorithms/main.cpp
+++ DataStructuresAndAlgorithms/main.cpp
@@ -1,22 +1,33 @@
 #include<iostream>
 #include "genericFunctions.hpp"
-#include<algorithm>
 using namespace std;
-
-int main(){
-    int arr[] = {3,5,2,1,6,9,8,7};
-    int n = sizeof(arr)/sizeof(int);
-    sort(arr,arr+n,greater<int>());
-    //compare is NOT a function call, instead passing a function as a perimeter
-    //greater<int>() is the change.
+void countingSort(int a[], int n){
+    //largest element
+    int largest = 0;
+    for(int i = 0; i<n; i++){
+        largest = max(largest, a[n]);
+    }
+    //Create a count array/vector
+    vector<int> freq(largest+1,0);
+    for(int i=0; i<n; i++){
+        freq[a[i]]++;
+    }
     
-    cout << "Sorted Array (in Reverse): " << endl;
-    printArray(arr,n);
-    
-    reverse(arr, arr+n);
-    cout << "Reversed Array: " << endl;
-    printArray(arr, n);
-    
-    return 0;
+    //Put back the elements
+    int j = 0;
+    for(int i=0; i<largest; i++){
+        while(freq[i] > 0){
+            a[j] = i;
+            freq[i]--;
+            j++;
+        }
+    }
+    return;
 }
 
+int main(){
+    int arr[] = {2,3,1,6,5,4,9,7,8};
+    int n = sizeof(arr)/sizeof(int);
+    countingSort(arr,n);
+    printArray(arr, n);
+}
